02fd482d6a37acccd53c946f050a35c58b3cab22
emmawirtt <75708036+emmawirtt@users.noreply.github.com>
2021-06-28 20:24:39 -0400

    Update script.js

--- _includes/script.js
+++ _includes/script.js
@@ -1,7 +1,11 @@
 cost tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
     const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
     target.classlist.add('active')
   })
 })
